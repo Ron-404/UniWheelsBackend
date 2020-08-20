13f8e2405e496e700b8efb74f097977ef21e341e
J. Eduardo Arias Barrera <julianeduardoarias@outlook.com>
2020-08-20 14:59:35 -0500

    DataBaseConection

--- src/main/java/edu/eci/ieti/UniwheelsApp.java
+++ src/main/java/edu/eci/ieti/UniwheelsApp.java
@@ -2,14 +2,20 @@ package edu.eci.ieti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.LoggerFactory;
 
 
 @SpringBootApplication
 public class UniwheelsApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {       
         SpringApplication.run(UniwheelsApp.class, args);
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        Logger rootLogger = loggerContext.getLogger("org.mongodb.driver");
+        rootLogger.setLevel(Level.OFF);
     }
 
 }
