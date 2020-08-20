ba4931a3b8fc1879f49d8489499cf20e469b5eeb
J. Eduardo Arias Barrera <julianeduardoarias@outlook.com>
2020-08-20 15:05:51 -0500

    Simplificar el truncado del logger

--- src/main/java/edu/eci/ieti/UniwheelsApp.java
+++ src/main/java/edu/eci/ieti/UniwheelsApp.java
@@ -3,7 +3,6 @@ package edu.eci.ieti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import org.slf4j.LoggerFactory;
 
@@ -13,9 +12,8 @@ public class UniwheelsApp {
 
     public static void main(String[] args) {       
         SpringApplication.run(UniwheelsApp.class, args);
-        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
-        Logger rootLogger = loggerContext.getLogger("org.mongodb.driver");
-        rootLogger.setLevel(Level.OFF);
+       ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger("org.mongodb.driver").setLevel(Level.WARN);
+
     }
 
 }
