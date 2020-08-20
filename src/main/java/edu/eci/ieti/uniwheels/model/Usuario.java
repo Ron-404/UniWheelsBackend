6828040e907f250b9eaa95897a33c2909f2ae33a
Camu10 <manuel.murillo.1999@gmail.com>
2020-08-19 22:21:52 -0500

    Model

---
+++ src/main/java/edu/eci/ieti/uniwheels/model/Usuario.java
@@ -0,0 +1,68 @@
+package edu.eci.ieti.uniwheels.model;
+
+public class Usuario {
+    public int userId;
+    public String username;
+    public String direccionResidencia;
+    public String password;
+    public String email;
+    public String universidad;
+
+    public Usuario(){}
+
+    public Usuario(String username,String password,String email, String universidad, String direccionResidencia){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.universidad = universidad;
+        this.direccionResidencia = direccionResidencia;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDireccionResidencia() {
+        return direccionResidencia;
+    }
+
+    public void setDireccionResidencia(String direccionResidencia) {
+        this.direccionResidencia = direccionResidencia;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUniversidad() {
+        return universidad;
+    }
+
+    public void setUniversidad(String universidad) {
+        this.universidad = universidad;
+    }
+}
