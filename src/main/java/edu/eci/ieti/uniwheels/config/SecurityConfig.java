89ec40e9efb2e520856f5ed51eb01fc904df270e
J. Eduardo Arias Barrera <julianeduardoarias@outlook.com>
2020-10-01 15:18:09 -0500

    Try Cors

--- src/main/java/edu/eci/ieti/uniwheels/config/SecurityConfig.java
+++ src/main/java/edu/eci/ieti/uniwheels/config/SecurityConfig.java
@@ -1,22 +1,13 @@
 package edu.eci.ieti.uniwheels.config;
 
-import edu.eci.ieti.uniwheels.services.AuthServices;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.activation.DataSource;
 
 @Configuration
 @EnableWebSecurity
---
+++ src/main/java/edu/eci/ieti/uniwheels/config/WebConfig.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.ieti.uniwheels.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+
+/**
+ *
+ * @author J. Eduardo Arias
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter{
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**");
+    }
+}
