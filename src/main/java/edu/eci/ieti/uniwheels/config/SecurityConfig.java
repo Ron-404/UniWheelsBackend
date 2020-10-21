effc0feeb496a63e8f161393379c32e5334afa68
Andres David Vasquez <andresdavid.vasquez@gmail.com>
2020-10-20 21:59:45 -0500

    Update SecurityConfig.java

--- src/main/java/edu/eci/ieti/uniwheels/config/SecurityConfig.java
+++ src/main/java/edu/eci/ieti/uniwheels/config/SecurityConfig.java
@@ -20,8 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-      //  http.csrf().disable().cors().and()
-        http.cors().and()
+        http.csrf().disable().cors().and()
                 .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/auth/login","/auth/addUser").permitAll()
